2fca25a5512fc34e58bfea6f1a705a82bf068078
Marco Marcellis <m.m.c.m.marcellis@hva.nl>
2018-07-09 10:49:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marmm/demolevel3/ReminderAdapter.java
@@ -0,0 +1,72 @@
+package com.example.marmm.demolevel3;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHolder> {
+
+    private List<Reminder> mReminders;
+    final private ReminderClickListener mReminderClickListener;
+
+    public interface ReminderClickListener{
+        void reminderOnClick (int i);
+    }
+
+
+    public ReminderAdapter(List<Reminder> mReminders, ReminderClickListener mReminderClickListener) {
+        this.mReminders = mReminders;
+        this.mReminderClickListener = mReminderClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ReminderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater= LayoutInflater.from(context);
+        View view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+// Return a new holder instance
+        ReminderAdapter.ViewHolder viewHolder = new ReminderAdapter.ViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReminderAdapter.ViewHolder holder, int position) {
+        Reminder reminder =  mReminders.get(position);
+        holder.textView.setText(reminder.getmReminderText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mReminders.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView textView;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+            textView=  itemView.findViewById(android.R.id.text1);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int clickedPosition = getAdapterPosition();
+            mReminderClickListener.reminderOnClick(clickedPosition);
+        }
+
+    }
+
+
+
+
+}
